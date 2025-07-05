a702795ada8a3c6c0fcc0848e5cc544b77c9bfe7
Sharvesh Ramesh <100819033+sharveshr@users.noreply.github.com>
2025-07-05 14:10:18 +0530

    Update Factorial.java

--- UseCase/Factorial.java
+++ UseCase/Factorial.java
@@ -7,11 +7,8 @@ public int factorial(int a) {
 		if(a<0) {
 			return -1;
 		}
-		else if(a==0) {
+		else if(a==0||a==1) {
 			return 1;
-		}
-		else if(a==1) {
-			return a*1;
 		}
 		else {
 			return a*factorial(a-1);
